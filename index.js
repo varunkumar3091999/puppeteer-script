2a5164bd6d7b8a245f1cb3d74601147fd6b8851e
varunkumar3091999 <varunkumar0930@gmail.com>
2020-08-03 15:25:18 +0400

    pdf generated

--- index.js
+++ index.js
@@ -8,17 +8,13 @@ const puppeteer = require("puppeteer-core");
         "use strict";
         const browser = await puppeteer.launch({
             executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-            headless: false,
+            // headless: false,
             defaultViewport: null
         });
         const page = await browser.newPage();
         await page.goto("https://github.com/varunkumar3091999");
         await page.screenshot({ path: "github.png", fullPage: true, omitBackground: true });
-        // await page.pdf({path: "github.pdf", format: "letter"})
-        // await page.pdf({
-        //     path: 'hn.pdf',
-        //     format: 'A4',
-        //   });
+        await page.pdf({path: "github.pdf", format: "A4", printBackground: true})
         await browser.close();
         console.log("puppeteer")
     }  
